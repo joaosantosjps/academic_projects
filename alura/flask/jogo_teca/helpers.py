955511d1ccdb17998b36aa250e20983c7052baf4
joaosantosjps <joaosantos270899@outlook.com>
2024-02-29 17:16:07 +0000

    Upload Studies: eliminando duplicata de capas ao atualizar a capa do jogo, criando def de eleminação

--- alura/flask/jogo_teca/helpers.py
+++ alura/flask/jogo_teca/helpers.py
@@ -7,3 +7,8 @@ def recupera_imagem(id):
             return nome_arquivo
     
     return "capa_padrão.jpg"
+
+def deleta_arquivo(id):
+    arquivo = recupera_imagem(id)
+    if arquivo != "capa_padrão.jpg":
+        os.remove(os.path.join(app.config["UPLOAD_PATH"], arquivo))
