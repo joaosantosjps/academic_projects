7987d0dd7eeee10fd89d2676d5e45029840640d5
joaosantosjps <joaosantos270899@outlook.com>
2024-07-10 22:50:03 +0100

    Criando Classe

--- alura/data_enginner/pipeline_dados/scripts/fusao_mercado_fevereiro.py
+++ alura/data_enginner/pipeline_dados/scripts/fusao_mercado_fevereiro.py
@@ -1,85 +1,47 @@
-import json
-import csv
-
-def leitura_json(path_json):
-    with open(path_json, 'r') as file:
-        dados_json = json.load(file)
-    
-    return dados_json
-
-def leitura_csv(path_csv):
-    dados_csv = []
-    
-    with open(path_csv, 'r') as file:
-        spamreader = csv.DictReader(file, delimiter=',')
-        for row in spamreader:
-            dados_csv.append(row)
-            
-    return dados_csv
-
-def leitura_dadods(path, tipo_arquivo):
-    dados = []
-    
-    if tipo_arquivo == "csv":
-        dados = leitura_csv(path)
-
-    elif tipo_arquivo == "json":
-        dados = leitura_json(path)
-
-    return dados
-
-def get_columns(dados):
-    return list(dados[0].keys())
-
-def remane_columns(dados, key_mapping):
-    new_dados_csv = []
-
-    for old_dict in dados:
-        dict_temp ={}
-
-        for old_key, value in old_dict.items():
-            dict_temp[key_mapping[old_key]] = value
-
-        new_dados_csv.append(dict_temp)
-
-    return new_dados_csv
-
-def size_data(dados):
-    return len(dados)
-
-def join(dadosA, dadosB):
-    combined_list = []
-
-    combined_list.extend(dadosA)
-    combined_list.extend(dadosB)
-    return combined_list
-
-def transformando_dados_tabela(dados, nome_colunas):
-    dados_combinado_tabela = [nome_colunas]
-
-    for row in dados:
-        linha = []
-
-    for coluna in nome_colunas:
-        linha.append(row.get(coluna, "Indisponivel"))
-    
-    dados_combinado_tabela.append(linha)
-    
-    return dados_combinado_tabela
-
-def salvando_dados(dados, path):
-
-    with open(path, "w") as file:
-        writer = csv.writer(file)
-        writer.writerows(dados)
+from pocessamento_dados import Dados
 
 path_json = "../data_raw/dados_empresaA.json"
 path_csv = "../data_raw/dados_empresaB.csv"
 
+# Extract
+
+dados_empresaA = Dados(path_json, "json")
+print(dados_empresaA.nome_colunas)
+
+dados_empresaB = Dados(path_csv, "csv")
+print(dados_empresaB.nome_colunas)
+
+# Transform
+
+key_mapping = {
+    "Nome do Item": "Nome do Produto",
+    "Classificação do Produto": "Categoria do Produto",
+    "Valor em Reais (R$)": "Preço do Produto (R$)",
+    "Quantidade em Estoque": "Quantidade em Estoque",
+    "Nome da Loja": "Filial",
+    "Data da Venda": "Data da Venda"
+}
+
+dados_empresaB.remane_columns(key_mapping)
+print(dados_empresaB.nome_colunas)
+
+print(dados_empresaA.qtd_linhas)
+print(dados_empresaB.qtd_linhas)
+
+dados_fusao = Dados.join(dados_empresaA, dados_empresaB)
+print(dados_fusao.nome_colunas)
+print(dados_fusao.qtd_linhas)
+
+# Load
+
+path_dados_combinado = "../data_processed/dados_combinados.csv"
+
+dados_fusao.salvando_dados(path_dados_combinado)
+print(path_dados_combinado)
 
 #Iniciando a leitura
 
-dados_json = leitura_dadods(path_json, "json")
+"""dados_json = leitura_dadods(path_json, "json")
 nome_colunas_json = get_columns(dados_json)
 tamanho_dados_json = size_data(dados_json)
 
@@ -92,16 +54,6 @@ tamanho_dados_csv = size_data(dados_csv)
 
 print(f"Nomes colunas dados csv: {nome_colunas_csv}")
 print(f"Tamanho dados csv: {tamanho_dados_csv}")
-
-key_mapping = {
-    "Nome do Item": "Nome do Produto",
-    "Classificação do Produto": "Categoria do Produto",
-    "Valor em Reais (R$)": "Preço do Produto (R$)",
-    "Quantidade em Estoque": "Quantidade em Estoque",
-    "Nome da Loja": "Filial",
-    "Data da Venda": "Data da Venda"
-}
-
 
 #Transformação dos dados
 
@@ -127,3 +79,4 @@ path_dados_combinado = "../data_processed/dados_combinados.csv"
 salvando_dados(dados_fusao_tabela, path_dados_combinado)
 
 print(path_dados_combinado)
+"""
