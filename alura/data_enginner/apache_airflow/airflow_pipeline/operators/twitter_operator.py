3289c28d532a687812d603fd43b04f8fc83b5d19
joaosantosjps <joaosantos270899@outlook.com>
2024-09-26 21:28:01 +0100

    Orquestração Airflow

--- alura/data_enginner/apache_airflow/airflow_pipeline/operators/twitter_operator.py
+++ alura/data_enginner/apache_airflow/airflow_pipeline/operators/twitter_operator.py
@@ -11,6 +11,8 @@ from airflow.models import BaseOperator, DAG, TaskInstance
 
 class TwitterOperator(BaseOperator):
 
+    tamplete_fields = ["query", "file_path", "start_time", "end_time"]
+
     def __init__(self, file_path, end_time, start_time, query, **kwargs):
         self.end_time = end_time
         self.start_time = start_time
