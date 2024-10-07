d3de4d6180d8f188ebdbe7886a63362087e81423
joaosantosjps <joaosantos270899@outlook.com>
2024-10-07 21:14:31 +0100

    Explorando com Spark

--- alura/data_enginner/apache_airflow/airflow_pipeline/hook/twitter_hook.py
+++ alura/data_enginner/apache_airflow/airflow_pipeline/hook/twitter_hook.py
@@ -1,20 +1,20 @@
-import json
+from airflow.providers.http.hooks.http import HttpHook
 import requests
 from datetime import datetime, timedelta
-from airflow.providers.http.hooks.http import HttpHook
+import json
 
 
 class TwitterHook(HttpHook):
 
-    def __init__(self, end_time, start_time, query, conn_id=None):
-        self.query = query
+    def __init__(self, end_time, start_time, query,  conn_id=None):
         self.end_time = end_time
         self.start_time = start_time
+        self.query = query
         self.conn_id = conn_id or "twitter_default"
-        super().__init__(http_conn_id=self.conn_id, method="GET")
+        super().__init__(http_conn_id=self.conn_id)
 
     def create_url(self):
-        
+
         TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
 
         end_time = self.end_time
@@ -27,15 +27,15 @@ class TwitterHook(HttpHook):
         url_raw = f"{self.base_url}/2/tweets/search/recent?query={query}&{tweet_fields}&{user_fields}&start_time={start_time}&end_time={end_time}"
 
         return url_raw
-    
+
     def connect_to_endpoint(self, url, session):
         request = requests.Request("GET", url)
         prep = session.prepare_request(request)
         self.log.info(f"URL: {url}")
-
         return self.run_and_check(session, prep, {})
-    
+
     def paginate(self, url_raw, session):
+        
         lista_json_response = []
         #imprimir json
         response = self.connect_to_endpoint(url_raw, session)
@@ -44,7 +44,7 @@ class TwitterHook(HttpHook):
         contador = 1
 
         # paginate
-        while "next_token" in json_response.get("meta",{}) and contador < 100:
+        while "next_token" in json_response.get("meta",{}) and contador<10:
             next_token = json_response['meta']['next_token']
             url = f"{url_raw}&next_token={next_token}"
             response = self.connect_to_endpoint(url, session)
@@ -61,11 +61,12 @@ class TwitterHook(HttpHook):
         return self.paginate(url_raw, session)
 
 if __name__ == "__main__":
+    #montando url
     TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
 
     end_time = datetime.now().strftime(TIMESTAMP_FORMAT)
     start_time = (datetime.now() + timedelta(-1)).date().strftime(TIMESTAMP_FORMAT)
     query = "datascience"
 
-    for pg in (TwitterHook(end_time, start_time, query).run()):
+    for pg in TwitterHook(end_time, start_time, query).run():
         print(json.dumps(pg, indent=4, sort_keys=True))
