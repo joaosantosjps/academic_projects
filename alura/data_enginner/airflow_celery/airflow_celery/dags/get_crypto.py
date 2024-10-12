b124244f14b019e1974b11a07019e8615d4488d2
joaosantosjps <joaosantos270899@outlook.com>
2024-10-12 12:13:27 +0100

    Paralelismo no Celery

--- alura/data_enginner/airflow_celery/airflow_celery/dags/get_crypto.py
+++ alura/data_enginner/airflow_celery/airflow_celery/dags/get_crypto.py
@@ -8,16 +8,16 @@ from airflow.decorators import dag, task
 
 
 TICKERS = [
-    "AAPL",
-    "MSFT",
-    "GOOG",
-    "TSLA"
+     "BTC",
+     "ETH",
+     "DOGE",
+     "AVAX"
 ]
 
 @task()
 
 def get_history(ticker, ds=None, ds_nodash=None):
-        file_path = f"/home/joao-santos/Desktop/academic_projects/alura/data_enginner/airflow_celery/stocks/{ticker}/{ticker}_{ds_nodash}.csv"
+        file_path = f"/home/joao-santos/Desktop/academic_projects/alura/data_enginner/airflow_celery/crypto/{ticker}/{ticker}_{ds_nodash}.csv"
         Path(file_path).parent.mkdir(parents=True, exist_ok=True)
 
         yfinance.Ticker(ticker).history(
@@ -31,13 +31,13 @@ def get_history(ticker, ds=None, ds_nodash=None):
         
 
 @dag(
-    schedule_interval="0 0 * * 2-6",
+    schedule_interval="0 0 * * *",
     start_date=pendulum.datetime(2024, 2, 1, tz="UTC"),
     catchup=True
 )
 
-def get_stocks_dag():
+def get_crypto_dag():
         for ticker in TICKERS:
             get_history.override(task_id=ticker, pool="small_pool")(ticker)
 
-dag = get_stocks_dag() 
+dag = get_crypto_dag()
